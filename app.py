526ab4f97e159056609bcf1e999c4769b50bf04e
Brian Sapozhnikov <brian.sapozhnikov@gmail.com>
2014-10-20 19:32:35 -0400

    added database methods to app.py for adding and getting things from blog.db

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ c = conn.cursor()
 app = Flask(__name__)
 
 ##attr should be a dictionary with attributes as the keys and types as the values
-##create_table('people',{'name':'text','id':'integer'})
 def create_table(name, attr):
     print name
     L = [k+' '+attr[k] for k in attr.keys()]
@@ -19,8 +18,29 @@ def create_table(name, attr):
 def drop_table(name):
     c.execute("DROP TABLE %s"%(name))
     conn.commit()
-    print ("DROP TABLE %s"%(name))    
+    print ("DROP TABLE %s"%(name))
 
+def add_post(title,cont):
+    c.execute("INSERT INTO posts VALUES (%s,%s)"%(title,cont))
+    conn.commit()
+    print "added %s to posts"%(title)
+
+def add_comment(post,cont):
+    c.execute("INSERT INTO comments VALUES (%s,%s)"%(post,cont))
+    conn.commit()
+    print "added comment to %s"%(post)
+
+##returns a dictionary of posts in the form 'title':'post'
+def get_posts():
+    posts={}
+    for row in c.execute("SELECT title FROM posts"):
+        posts[row[0]]=row[1]
+    return posts
+
+##return a list of comments for the given posts
+def get_comments(post):
+    return [row[0] for row in c.execute("SELECT comment FROM comments WHERE post=?",(post,))]
+    
 @app.route('/', methods=['GET','POST'])
 def index():
     if request.method=='GET':
@@ -29,6 +49,9 @@ def index():
         #validate and add post to blog
         title = request.form['title']
         post = request.form['post']
+        #Currently, there is no validation of the title/post
+        add_post(title,post)
+        
         
 
 @app.route('/<title>', methods=['GET','POST'])
@@ -40,7 +63,8 @@ def show_post(title):
         #add comment to post
         #show post
         comment = request.form['comment']
-
+        add_comment(title,comment)
+        
 @app.route("/resetdb")
 def create_tables():
     drop_table('posts')
@@ -52,6 +76,5 @@ def create_tables():
 if __name__=='__main__':
     app.debug=True
     app.run()
-    #drop_table('posts')
-    #drop_table('comments')
+  
     
